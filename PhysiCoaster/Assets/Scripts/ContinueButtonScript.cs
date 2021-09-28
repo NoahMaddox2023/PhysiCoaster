b453f276728a234dab2a92c5da9fce1fa17f16e2
QuesadillaBomb <noahjmaddox@charter.net>
2021-09-27 21:38:20 -0400

    Added tutorials
    Finished up the main menu functionality. Start game, exit game, and the tutorials are finished, until we want to add or change them.

---
+++ PhysiCoaster/Assets/Scripts/ContinueButtonScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueButtonScript : MonoBehaviour
+{
+    public void ContinueButton()
+    {
+        SceneManager.LoadScene("Tutorial2");
+    }
+}

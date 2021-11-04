003ad016a3d8be4366789fe7c574707a988fff0a
QuesadillaBomb <noahjmaddox@charter.net>
2021-11-04 14:38:30 -0400

    Fixed Q key being checked

--- PhysiCoaster/Assets/Scripts/RideTrack.cs
+++ PhysiCoaster/Assets/Scripts/RideTrack.cs
@@ -17,7 +17,8 @@ public class RideTrack : MonoBehaviour
     public float gravity;
     private float velocity;
     public float coefFriction;
-    public float mass; 
+    public float mass;
+    private bool QKeyPressed;
     public bool madeDestination;
     public Text levelClearText;
     public Button resultsScreenButton;
@@ -51,13 +52,21 @@ public class RideTrack : MonoBehaviour
         };
         rayTime = 0;
     }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            QKeyPressed = !QKeyPressed;
+        }
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         velocity -= velocity * Time.deltaTime * coefFriction;
         transform.position += (transform.right * velocity * Time.deltaTime);
         //Debug.Log("Velocity is: " + velocity);
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (QKeyPressed)
         {
             GetComponent<Rigidbody>().velocity = transform.right * speed;
             move = !move;

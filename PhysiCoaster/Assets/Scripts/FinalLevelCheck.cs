c4bc15e69be5d7fb7b443100765dd27cb0baa7ea
QuesadillaBomb <noahjmaddox@charter.net>
2021-12-07 12:07:46 -0500

    Removed next level button
    • Removed next level button on final results screen

---
+++ PhysiCoaster/Assets/Scripts/FinalLevelCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalLevelCheck : MonoBehaviour
+{
+    public Button nextLevelButton;
+    public GameObject nextLevelButtonGameObject;
+    void Start()
+    {
+        Debug.Log(PlayerPrefs.GetInt("NextLevel"));
+        if (PlayerPrefs.GetInt("NextLevel") > 14)
+        {
+            nextLevelButton.enabled = false;
+            nextLevelButtonGameObject.SetActive(false);
+        }
+    }
+}
